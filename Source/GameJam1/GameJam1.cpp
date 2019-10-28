ff31deb5820156b3418537a628d151e857580ce0
JamesMcAlenyWork <james_mcaleny_work@hotmail.com>
2019-10-28 14:38:12 +0000

    Added C++ tests(For Jay)

---
+++ Source/GameJam1/GameJam1.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GameJam1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameJam1, "GameJam1" );
